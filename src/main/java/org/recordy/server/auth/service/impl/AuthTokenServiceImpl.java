a759f2ac52ab778cc49bb2968d345663d0a002ad
Jin Geonwoo <jinkonu99@gmail.com>
2024-06-25 01:45:52 +0900

    [fix] AuthTokenServiceImpl 내 오버라이딩 추가 (#1)

--- src/main/java/org/recordy/server/auth/service/impl/AuthTokenServiceImpl.java
+++ src/main/java/org/recordy/server/auth/service/impl/AuthTokenServiceImpl.java
@@ -3,6 +3,7 @@ package org.recordy.server.auth.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.recordy.server.auth.domain.AuthToken;
 import org.recordy.server.auth.service.AuthTokenService;
+import org.recordy.server.auth.service.dto.AuthTokenValidationResult;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -11,6 +12,11 @@ public class AuthTokenServiceImpl implements AuthTokenService {
 
     @Override
     public AuthToken issueToken(long userId) {
+        return null;
+    }
+
+    @Override
+    public AuthTokenValidationResult validateToken(String token) {
         return null;
     }
 }
