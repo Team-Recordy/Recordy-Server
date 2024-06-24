713ae65af39c6cba2c432aff919e1324e0b8555b
Jin Geonwoo <jinkonu99@gmail.com>
2024-06-24 16:03:57 +0900

    [refactor] 리포지토리와 연관된 엔티티 로직 추가 (#1)

--- src/main/java/org/recordy/server/auth/domain/AuthEntity.java
+++ src/main/java/org/recordy/server/auth/domain/AuthEntity.java
@@ -1,14 +1,38 @@
 package org.recordy.server.auth.domain;
 
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.recordy.server.common.domain.JpaMetaInfoEntity;
+import org.springframework.data.redis.core.RedisHash;
 
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@RedisHash("auth")
 public class AuthEntity extends JpaMetaInfoEntity {
 
+    @Id
+    private String platformId;
+    private String platformType;
+    private String accessToken;
+    private String refreshToken;
+
     public static AuthEntity from(Auth auth) {
-        return null;
+        return new AuthEntity(
+                auth.getPlatform().getId(),
+                auth.getPlatform().getType().name(),
+                auth.getToken().getAccessToken(),
+                auth.getToken().getRefreshToken()
+        );
     }
 
     public Auth toDomain() {
-        return null;
+        return new Auth(
+                new AuthPlatform(platformId, AuthPlatform.Type.valueOf(platformType)),
+                new AuthToken(accessToken, refreshToken)
+        );
     }
 }
