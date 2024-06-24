5ef8cd77a033be05961c4213e7316bcfeca53423
Jin Geonwoo <jinkonu99@gmail.com>
2024-06-25 01:17:39 +0900

    [feat] 토큰 validation 결과 enum 및 validation 메서드 추가 (#1)

--- src/main/java/org/recordy/server/auth/service/AuthTokenService.java
+++ src/main/java/org/recordy/server/auth/service/AuthTokenService.java
@@ -1,8 +1,10 @@
 package org.recordy.server.auth.service;
 
 import org.recordy.server.auth.domain.AuthToken;
+import org.recordy.server.auth.service.dto.AuthTokenValidationResult;
 
 public interface AuthTokenService {
 
     AuthToken issueToken(long userId);
+    AuthTokenValidationResult validateToken(String token);
 }
---
+++ src/main/java/org/recordy/server/auth/service/dto/AuthTokenValidationResult.java
@@ -0,0 +1,10 @@
+package org.recordy.server.auth.service.dto;
+
+public enum AuthTokenValidationResult {
+    VALID_JWT,              // 유효한 토큰
+    INVALID_SIGNATURE,      // 유효하지 않은 서명
+    INVALID_TOKEN,          // 유효하지 않은 토큰
+    EXPIRED_TOKEN,          // 만료된 토큰
+    UNSUPPORTED_TOKEN,      // 지원하지 않는 형식의 토큰
+    EMPTY_TOKEN             // 빈 토큰
+}
