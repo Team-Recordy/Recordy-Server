34e83625b13f16cc777f217ca1919a9c6653545c
Jin Geonwoo <jinkonu99@gmail.com>
2024-06-24 14:59:42 +0900

    [feat] JPA 엔티티 추적을 위한 기본 엔티티 및 config 파일 추가 (#1)

---
+++ src/main/java/org/recordy/server/common/config/JpaEntityAuditingConfig.java
@@ -0,0 +1,9 @@
+package org.recordy.server.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaEntityAuditingConfig {
+}
