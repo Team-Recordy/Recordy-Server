114cd649f34834e85199fce9f13161a55a580da6
Jin Geonwoo <jinkonu99@gmail.com>
2024-06-24 16:03:31 +0900

    [refactor] Auth 도메인에서 id, User 제거 (#1)

--- src/main/java/org/recordy/server/auth/domain/Auth.java
+++ src/main/java/org/recordy/server/auth/domain/Auth.java
@@ -2,14 +2,11 @@ package org.recordy.server.auth.domain;
 
 import lombok.Builder;
 import lombok.Getter;
-import org.recordy.server.user.domain.User;
 
 @Builder
 @Getter
 public class Auth {
 
-    private String id;
     private AuthPlatform platform;
     private AuthToken token;
-    private User user;
 }
