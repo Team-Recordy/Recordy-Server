2f891e87590db45920f9d29d33389a333f7b411a
Jin Geonwoo <jinkonu99@gmail.com>
2024-06-26 00:46:26 +0900

    [test] AuthServiceIntegrationTest 수정 (#1)

--- src/test/java/org/recordy/server/auth/service/AuthServiceIntegrationTest.java
+++ src/test/java/org/recordy/server/auth/service/AuthServiceIntegrationTest.java
@@ -56,8 +56,7 @@ class AuthServiceIntegrationTest {
                 .thenReturn(DomainFixture.createAuthToken());
     }
 
-    //TODO: 클라이언트 측에서 애플 로그인 구현한 이후에 테스트 가능
-//    @Test
+    @Test
     void signIn을_요청한_가입되지_않은_사용자에_대해_새로운_User_객체가_저장된다() {
         // given
         AuthPlatform.Type platformType = DomainFixture.PLATFORM_TYPE;
@@ -76,7 +75,7 @@ class AuthServiceIntegrationTest {
         );
     }
 
-//    @Test
+    @Test
     void signIn을_요청한_가입되지_않은_사용자는_isSignedUp이_false인_Auth_객체를_반환한다() {
         // given
         AuthPlatform.Type platformType = DomainFixture.PLATFORM_TYPE;
@@ -91,7 +90,7 @@ class AuthServiceIntegrationTest {
         );
     }
 
-//    @Test
+    @Test
     void signIn을_요청한_가입되지_않은_사용자는_PENDING_상태로_가입_처리된다() {
         // given
         AuthPlatform.Type platformType = DomainFixture.PLATFORM_TYPE;
@@ -110,7 +109,7 @@ class AuthServiceIntegrationTest {
         );
     }
 
-//    @Test
+    @Test
     void signIn을_요청한_이미_가입된_사용자는_isSignedUp이_true인_Auth_객체를_반환한다() {
         // given
         AuthPlatform.Type platformType = DomainFixture.PLATFORM_TYPE;
@@ -127,7 +126,7 @@ class AuthServiceIntegrationTest {
         );
     }
 
-//    @Test
+    @Test
     void signIn을_요청한_이미_가입된_사용자는_ACTIVE_상태다() {
         // given
         AuthPlatform.Type platformType = DomainFixture.PLATFORM_TYPE;
