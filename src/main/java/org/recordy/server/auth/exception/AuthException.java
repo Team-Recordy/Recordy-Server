82b3d95406d8124130c5a59e30641d9f7333ff38
elive7 <parksub020704@gmail.com>
2024-06-27 02:58:36 +0900

    [feat] Apple Login에 필요한 AuthException 및 에러 메세지 정의 (#7)

---
+++ src/main/java/org/recordy/server/auth/exception/AuthException.java
@@ -0,0 +1,12 @@
+package org.recordy.server.auth.exception;
+
+import org.recordy.server.auth.message.ErrorMessage;
+
+public class AuthException extends RuntimeException{
+    private final ErrorMessage errorMessage;
+    public AuthException(ErrorMessage errorMessage){
+        super(errorMessage.getMessage());
+        this.errorMessage = errorMessage;
+    }
+
+}
