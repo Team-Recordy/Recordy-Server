ffad966adf34c28628fbfd2f8c3ce03dda4bf02d
sebbbin <nsb08045@daum.net>
2024-06-26 14:21:42 +0900

    Revert "[feat] UserController  swagger설정 (#5)"
    This reverts commit 647a366ab843c6679dfe3beb756ae1bb02ceecc4.

--- src/main/java/org/recordy/server/user/controller/UserApi.java
+++ src/main/java/org/recordy/server/user/controller/UserApi.java
@@ -7,11 +7,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.recordy.server.user.controller.dto.request.UserSignInRequest;
 import org.recordy.server.user.controller.dto.response.UserSignInResponse;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 
 @Tag(name = "유저 관련 API")
 public interface UserApi {
@@ -22,18 +19,15 @@ public interface UserApi {
             responses = {
                     @ApiResponse(
                             responseCode = "200",
-                            description = "회원가입 성공",
+                            description = "성공",
                             content = @Content(
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
                                     schema = @Schema(
-                                            implementation = UserSignInResponse.class
+                                            implementation = UserSignInRequest.class
                                     )
                             )
                     )
             }
     )
-    ResponseEntity<UserSignInResponse> signIn(
-            @RequestHeader(HttpHeaders.AUTHORIZATION) String platformToken,
-            @RequestBody UserSignInRequest request
-    );
+    public ResponseEntity<UserSignInResponse> signIn();
 }
