17231208673499aff4be9256a6d137b89db7a805
elive7 <parksub020704@gmail.com>
2024-06-27 03:06:51 +0900

    [feat] AppleOAuthUserProvider 정의 및 getPlatform 로직 구현 (#7)

--- src/main/java/org/recordy/server/auth/service/impl/apple/AuthApplePlatformServiceImpl.java
+++ src/main/java/org/recordy/server/auth/service/impl/apple/AuthApplePlatformServiceImpl.java
@@ -2,6 +2,7 @@ package org.recordy.server.auth.service.impl.apple;
 
 import lombok.RequiredArgsConstructor;
 import org.recordy.server.auth.domain.AuthPlatform;
+import org.recordy.server.auth.domain.AuthPlatform.Type;
 import org.recordy.server.auth.domain.usecase.AuthSignIn;
 import org.recordy.server.auth.service.AuthPlatformService;
 import org.springframework.stereotype.Service;
@@ -9,10 +10,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class AuthApplePlatformServiceImpl implements AuthPlatformService {
+    public final AppleOAuthUserProvider appleOAuthUserProvider;
 
     @Override
     public AuthPlatform getPlatform(AuthSignIn authSignIn) {
-        return null;
+        String identityToken = authSignIn.platformToken();
+        String id = appleOAuthUserProvider.getApplePlatformId(identityToken);
+        return new AuthPlatform(id, Type.APPLE);
     }
 
     @Override
