1982195f53e8637898186c1108749abe00402f69
sebbbin <nsb08045@daum.net>
2024-06-26 14:27:22 +0900

    Revert "[feat] 유저 회원가입 api 인터페이스 추가"
    This reverts commit b7beba4ed259a75c80fc797febe31d691b79652b.

--- src/main/java/org/recordy/server/user/controller/UserApi.java
+++ src/main/java/org/recordy/server/user/controller/UserApi.java
@@ -1,33 +1,4 @@
 package org.recordy.server.user.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.recordy.server.user.controller.dto.request.UserSignInRequest;
-import org.recordy.server.user.controller.dto.response.UserSignInResponse;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-
-@Tag(name = "유저 관련 API")
 public interface UserApi {
-
-    @Operation(
-            summary = "유저 회원가입 API",
-            description = "유저가 회원가입하는 API입니다.",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "성공",
-                            content = @Content(
-                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                                    schema = @Schema(
-                                            implementation = UserSignInRequest.class
-                                    )
-                            )
-                    )
-            }
-    )
-    public ResponseEntity<UserSignInResponse> signIn();
 }
