606f0694956b323f262207f45a6d6c6a941dd8d9
sebbbin <nsb08045@daum.net>
2024-06-26 12:58:19 +0900

    [feat] UserController  swagger설정 (#5)

--- src/main/java/org/recordy/server/user/controller/UserApi.java
+++ src/main/java/org/recordy/server/user/controller/UserApi.java
@@ -7,8 +7,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.recordy.server.user.controller.dto.request.UserSignInRequest;
 import org.recordy.server.user.controller.dto.response.UserSignInResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @Tag(name = "유저 관련 API")
 public interface UserApi {
@@ -19,15 +22,18 @@ public interface UserApi {
             responses = {
                     @ApiResponse(
                             responseCode = "200",
-                            description = "성공",
+                            description = "회원가입 성공",
                             content = @Content(
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
                                     schema = @Schema(
-                                            implementation = UserSignInRequest.class
+                                            implementation = UserSignInResponse.class
                                     )
                             )
                     )
             }
     )
-    public ResponseEntity<UserSignInResponse> signIn();
+    ResponseEntity<UserSignInResponse> signIn(
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String platformToken,
+            @RequestBody UserSignInRequest request
+    );
 }
