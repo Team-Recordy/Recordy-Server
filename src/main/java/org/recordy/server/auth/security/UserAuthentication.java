31d6c03ddf481e2d715c66ecf880fcb08c3fa3fe
Jin Geonwoo <jinkonu99@gmail.com>
2024-06-27 21:09:24 +0900

    [feat] UserAuthentication 파일 추가 (#4)

---
+++ src/main/java/org/recordy/server/auth/security/UserAuthentication.java
@@ -0,0 +1,17 @@
+package org.recordy.server.auth.security;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class UserAuthentication extends UsernamePasswordAuthenticationToken {
+
+    public UserAuthentication(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities) {
+        super(principal, credentials, authorities);
+    }
+
+    public static UserAuthentication of(Long id) {
+        return new UserAuthentication(id, null, null);
+    }
+}
