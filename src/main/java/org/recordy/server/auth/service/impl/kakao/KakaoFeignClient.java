68bdc36952467d874a4cb8f42652b1691fd0fb58
sebbbin <nsb08045@daum.net>
2024-06-27 20:48:19 +0900

    [fix] FeignClient 간접 주입(#10)

--- src/main/java/org/recordy/server/auth/service/impl/kakao/KakaoFeignClient.java
+++ src/main/java/org/recordy/server/auth/service/impl/kakao/KakaoFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "kakao-feign-client", url = "https://kapi.kakao.com/")
+@FeignClient(name = "${auth.oauth.kakao.name}", url = "${auth.oauth.kakao.url}")
 public interface KakaoFeignClient {
     @GetMapping("v1/user/access_token_info")
     KakaoAccessPlatformInfo getKakaoAccessTokenInfo(@RequestHeader("Authorization") String accessToken);
