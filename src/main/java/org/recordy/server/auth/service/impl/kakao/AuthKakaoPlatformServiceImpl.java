967129653775d1a387e89183e2ed9001549c65e0
sebbbin <nsb08045@daum.net>
2024-06-27 11:13:28 +0900

    [feat] getPlatform 매서드 정의 (#10)

--- src/main/java/org/recordy/server/auth/service/impl/kakao/AuthKakaoPlatformServiceImpl.java
+++ src/main/java/org/recordy/server/auth/service/impl/kakao/AuthKakaoPlatformServiceImpl.java
@@ -1,18 +1,31 @@
 package org.recordy.server.auth.service.impl.kakao;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.recordy.server.auth.domain.AuthPlatform;
 import org.recordy.server.auth.domain.usecase.AuthSignIn;
+import org.recordy.server.auth.exception.ErrorMessage;
+import org.recordy.server.auth.exception.UnauthorizedException;
+import org.recordy.server.auth.kakao.KakaoOAuthProvider;
 import org.recordy.server.auth.service.AuthPlatformService;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class AuthKakaoPlatformServiceImpl implements AuthPlatformService {
 
+    private final KakaoOAuthProvider kakaoOAuthProvider;
+
     @Override
     public AuthPlatform getPlatform(AuthSignIn authSignIn) {
-        return null;
+        try {
+            String platformId = kakaoOAuthProvider.getKakaoPlatformId(authSignIn.platformToken());
+            return new AuthPlatform(platformId, getPlatformType());
+        } catch (UnauthorizedException e) {
+            log.error("Failed to get Kakao platform id", e);
+            throw new UnauthorizedException(ErrorMessage.INVALID_KAKAO_ACCESS_TOKEN);
+        }
     }
 
     @Override
