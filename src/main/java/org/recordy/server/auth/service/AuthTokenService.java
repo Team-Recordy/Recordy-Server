9a93cae2609f064309cf311f7407d756b6394358
Jin Geonwoo <jinkonu99@gmail.com>
2024-06-28 16:05:38 +0900

    [feat] AuthTokenService 내 HTTP 요청에서 토큰 파싱하는 행동 추가 (#17)

--- src/main/java/org/recordy/server/auth/service/AuthTokenService.java
+++ src/main/java/org/recordy/server/auth/service/AuthTokenService.java
@@ -1,5 +1,6 @@
 package org.recordy.server.auth.service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.recordy.server.auth.domain.AuthToken;
 import org.recordy.server.auth.service.dto.AuthTokenValidationResult;
 
@@ -7,4 +8,5 @@ public interface AuthTokenService {
 
     AuthToken issueToken(long userId);
     AuthTokenValidationResult validateToken(String token);
+    String getTokenFromRequest(HttpServletRequest request);
 }
--- src/main/java/org/recordy/server/auth/service/impl/AuthTokenServiceImpl.java
+++ src/main/java/org/recordy/server/auth/service/impl/AuthTokenServiceImpl.java
@@ -2,18 +2,23 @@ package org.recordy.server.auth.service.impl;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.recordy.server.auth.domain.AuthToken;
+import org.recordy.server.auth.exception.AuthException;
+import org.recordy.server.auth.message.ErrorMessage;
 import org.recordy.server.auth.security.UserAuthentication;
 import org.recordy.server.auth.service.AuthTokenService;
 import org.recordy.server.auth.service.dto.AuthTokenValidationResult;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Base64;
 import java.util.Date;
 import java.util.Map;
+import java.util.Optional;
 
 import static org.recordy.server.auth.service.dto.AuthTokenValidationResult.*;
 
@@ -22,19 +27,21 @@ import static org.recordy.server.auth.service.dto.AuthTokenValidationResult.*;
 public class AuthTokenServiceImpl implements AuthTokenService {
 
     @Value("${auth.token.secret}")
-    private static String SECRET;
+    private String SECRET;
     @Value("${auth.token.access.expiration}")
-    private static long ACCESS_TOKEN_EXPIRATION;
+    private long ACCESS_TOKEN_EXPIRATION;
     @Value("${auth.token.refresh.expiration}")
-    private static long REFRESH_TOKEN_EXPIRATION;
+    private long REFRESH_TOKEN_EXPIRATION;
     @Value("${auth.token.access.type}")
-    private static String ACCESS_TOKEN_TYPE;
+    private String ACCESS_TOKEN_TYPE;
     @Value("${auth.token.refresh.type}")
-    private static String REFRESH_TOKEN_TYPE;
+    private String REFRESH_TOKEN_TYPE;
     @Value("${auth.token.key.user_id}")
-    private static String USER_ID;
+    private String USER_ID;
     @Value("${auth.token.key.token_type}")
-    private static String TOKEN_TYPE;
+    private String TOKEN_TYPE;
+    @Value("${auth.token.prefix}")
+    private String TOKEN_PREFIX;
 
     @Override
     public AuthToken issueToken(long userId) {
@@ -44,6 +51,31 @@ public class AuthTokenServiceImpl implements AuthTokenService {
                 generateToken(authentication, ACCESS_TOKEN_EXPIRATION, ACCESS_TOKEN_TYPE),
                 generateToken(authentication, REFRESH_TOKEN_EXPIRATION, REFRESH_TOKEN_TYPE)
         );
+    }
+
+    @Override
+    public AuthTokenValidationResult validateToken(String token) {
+        try {
+            getBody(token);
+
+            return VALID_JWT;
+        } catch (MalformedJwtException ex) {
+            return INVALID_TOKEN;
+        } catch (ExpiredJwtException ex) {
+            return EXPIRED_TOKEN;
+        } catch (UnsupportedJwtException ex) {
+            return UNSUPPORTED_TOKEN;
+        } catch (IllegalArgumentException ex) {
+            return EMPTY_TOKEN;
+        }
+    }
+
+    @Override
+    public String getTokenFromRequest(HttpServletRequest request) {
+        return Optional.ofNullable(request.getHeader(HttpHeaders.AUTHORIZATION))
+                .filter(t -> t.startsWith(TOKEN_PREFIX))
+                .map(t -> t.replace(TOKEN_PREFIX.concat(" "), ""))
+                .orElseThrow(() -> new AuthException(ErrorMessage.INVALID_ACCESS_TOKEN));
     }
 
     private String generateToken(UserAuthentication authentication, long expiration, String tokenType) {
@@ -64,23 +96,6 @@ public class AuthTokenServiceImpl implements AuthTokenService {
         claims.putAll(values);
 
         return claims;
-    }
-
-    @Override
-    public AuthTokenValidationResult validateToken(String token) {
-        try {
-            getBody(token);
-
-            return VALID_JWT;
-        } catch (MalformedJwtException ex) {
-            return INVALID_TOKEN;
-        } catch (ExpiredJwtException ex) {
-            return EXPIRED_TOKEN;
-        } catch (UnsupportedJwtException ex) {
-            return UNSUPPORTED_TOKEN;
-        } catch (IllegalArgumentException ex) {
-            return EMPTY_TOKEN;
-        }
     }
 
     private Claims getBody(String token) {
