c1ad7d655ad82fa482f7f76b33c0a156f7d2bec2
sebbbin <nsb08045@daum.net>
2024-06-30 22:04:00 +0900

    [fix] 필요없는 어노테이션 삭제(#19)

--- src/main/java/org/recordy/server/ServerApplication.java
+++ src/main/java/org/recordy/server/ServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "org.recordy.server.auth.kakao")
 public class ServerApplication {
 
     public static void main(String[] args) {
