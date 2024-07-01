d20b0a82df6fd3e1a9409fc4e8350339a25cfc53
Jin Geonwoo <jinkonu99@gmail.com>
2024-07-01 19:10:14 +0900

    [feat] 예외 핸들러 추가 (#22)

---
+++ src/main/java/org/recordy/server/common/handler/GlobalExceptionHandler.java
@@ -0,0 +1,88 @@
+package org.recordy.server.common.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.recordy.server.common.dto.response.ErrorResponse;
+import org.recordy.server.common.exception.RecordyException;
+import org.recordy.server.common.message.ErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.Objects;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // COMMON - CLIENT ERROR
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(final HttpMessageNotReadableException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ErrorResponse.of(ErrorMessage.ENUM_VALUE_BAD_REQUEST));
+    }
+
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ResponseEntity<ErrorResponse> handleHandlerMethodValidationException(final HandlerMethodValidationException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ErrorResponse.of(
+                        HttpStatus.BAD_REQUEST.value(),
+                        Objects.requireNonNull(e.getAllValidationResults().get(0).getResolvableErrors().get(0).getDefaultMessage()))
+                );
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(final MethodArgumentTypeMismatchException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ErrorResponse.of(ErrorMessage.INVALID_REQUEST_BODY_TYPE));
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(final HttpRequestMethodNotSupportedException e) {
+        return ResponseEntity
+                .status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body(ErrorResponse.of(ErrorMessage.METHOD_NOT_SUPPORTED));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        FieldError fieldError = e.getBindingResult().getFieldError();
+
+        if (Objects.isNull(fieldError))
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(ErrorResponse.of(ErrorMessage.VALIDATION_REQUEST_MISSING_EXCEPTION));
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ErrorResponse.of(HttpStatus.BAD_REQUEST.value(), fieldError.getDefaultMessage()));
+    }
+
+    // COMMON - SERVER ERROR
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(final Exception error) {
+        log.error("================================================NEW===============================================");
+        log.error(error.getMessage(), error);
+
+        return ResponseEntity
+                .status(ErrorMessage.INTERNAL_SERVER_ERROR.getHttpStatus())
+                .body(ErrorResponse.of(ErrorMessage.INTERNAL_SERVER_ERROR));
+    }
+
+    // DOMAIN
+    @ExceptionHandler(RecordyException.class)
+    public ResponseEntity<ErrorResponse> handleDomainException(final RecordyException e) {
+        return ResponseEntity
+                .status(e.getErrorMessage().getHttpStatus())
+                .body(ErrorResponse.of(e.getErrorMessage()));
+    }
+}
