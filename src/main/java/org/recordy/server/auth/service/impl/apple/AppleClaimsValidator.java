9b55288de48195c41a6ef2e82044da184c1fae94
elive7 <parksub020704@gmail.com>
2024-07-01 20:17:30 +0900

    [feat] 검증 과정에서 nonce 값 삭제 (#7)

--- src/main/java/org/recordy/server/auth/service/impl/apple/AppleClaimsValidator.java
+++ src/main/java/org/recordy/server/auth/service/impl/apple/AppleClaimsValidator.java
@@ -12,23 +12,18 @@ public class AppleClaimsValidator {
     private static final String NONCE_KEY = "nonce";
     private String iss;
     private String clientId;
-    private String nonce;
 
     public AppleClaimsValidator(
             @Value("${auth.oauth.apple.iss}") String iss,
-            @Value("${auth.oauth.apple.client-id}") String clientId,
-            @Value("${auth.oauth.apple.nonce}") String nonce
+            @Value("${auth.oauth.apple.client-id}") String clientId
     ) {
         this.iss = iss;
         this.clientId = clientId;
-        this.nonce = EncryptUtils.encrypt(nonce);
     }
 
     public void validate(Claims claims) {
-        this.nonce = EncryptUtils.encrypt(nonce);
         if (!(claims.getIssuer().contains(iss) &&
-                claims.getAudience().equals(clientId) &&
-                claims.get(NONCE_KEY, String.class).equals(nonce))){
+                claims.getAudience().equals(clientId))){
             throw new AuthException(ErrorMessage.APPLE_INVALID_IDENTITY_TOKEN_CLAIMS);
         }
     }
