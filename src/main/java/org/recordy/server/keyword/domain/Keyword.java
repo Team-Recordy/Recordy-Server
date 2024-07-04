4c5b299f855e2e700fa069ef32e287fdca5e221a
Jin Geonwoo <jinkonu99@gmail.com>
2024-07-04 18:06:11 +0900

    [refactor] 키워드에서 id 제거 (#29)

--- src/main/java/org/recordy/server/keyword/domain/Keyword.java
+++ src/main/java/org/recordy/server/keyword/domain/Keyword.java
@@ -2,28 +2,18 @@ package org.recordy.server.keyword.domain;
 
 public enum Keyword {
 
-    EXOTIC(1),
-    QUITE(2),
-    TRENDY(3),
-    SENSUAL(4),
-    CUTE(5),
-    DUCKUMORI(6),
-    FUNNY(7),
-    COZY(8),
-    CLASSIC(9),
-    GOOD_TO_FOCUS(10),
-    CLEAN(11),
-    SCARY(12),
-    VIBRANT(13),
+    EXOTIC,
+    QUITE,
+    TRENDY,
+    SENSUAL,
+    CUTE,
+    DUCKUMORI,
+    FUNNY,
+    COZY,
+    CLASSIC,
+    GOOD_TO_FOCUS,
+    CLEAN,
+    SCARY,
+    VIBRANT,
     ;
-
-    private final int id;
-
-    Keyword(int id) {
-        this.id = id;
-    }
-
-    public int getId() {
-        return id;
-    }
 }
