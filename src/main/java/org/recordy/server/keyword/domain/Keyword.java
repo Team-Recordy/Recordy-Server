6f0a4905b2dd1ef6f4f532e93ebe3868b45729cc
Jin Geonwoo <jinkonu99@gmail.com>
2024-07-04 17:50:50 +0900

    [feat] 키워드 내 id 필드 추가 (#29)

--- src/main/java/org/recordy/server/keyword/domain/Keyword.java
+++ src/main/java/org/recordy/server/keyword/domain/Keyword.java
@@ -2,18 +2,28 @@ package org.recordy.server.keyword.domain;
 
 public enum Keyword {
 
-    EXOTIC,
-    QUITE,
-    TRENDY,
-    SENSUAL,
-    CUTE,
-    DUCKUMORI,
-    FUNNY,
-    COZY,
-    CLASSIC,
-    GOOD_TO_FOCUS,
-    CLEAN,
-    SCARY,
-    VIBRANT,
+    EXOTIC(1),
+    QUITE(2),
+    TRENDY(3),
+    SENSUAL(4),
+    CUTE(5),
+    DUCKUMORI(6),
+    FUNNY(7),
+    COZY(8),
+    CLASSIC(9),
+    GOOD_TO_FOCUS(10),
+    CLEAN(11),
+    SCARY(12),
+    VIBRANT(13),
     ;
+
+    private final int id;
+
+    Keyword(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
