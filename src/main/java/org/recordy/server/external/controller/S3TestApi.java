e57118786db590f9ce6d9f61ae2586b6baab0944
sebbbin <nsb08045@daum.net>
2024-07-08 12:12:30 +0900

    [refactor] 자동정렬 (#32)

--- src/main/java/org/recordy/server/external/controller/S3TestApi.java
+++ src/main/java/org/recordy/server/external/controller/S3TestApi.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 
 @Tag(name = "S3 테스트 관련 API")
 public interface S3TestApi {
+
     @Operation(
             summary = "이미지 업로드 테스트 API",
             description = "S3에 이미지가 업로드 되는지 테스트하는 API입니다. 이미지 사이즈는 5MB를 넘을 수 없습니다. 이미지 확장자는 jpg, png, webp만 가능합니다.",
