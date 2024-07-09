9b8e9dfd3bbf2552453f351856a7add54f4f4bce
elive7 <parksub020704@gmail.com>
2024-07-09 21:33:08 +0900

    [feat] bookmark, getBookmarkedRecords, getBookmarkCount 로직 구현 (#41)

--- src/main/java/org/recordy/server/record_stat/repository/BookmarkJpaRepository.java
+++ src/main/java/org/recordy/server/record_stat/repository/BookmarkJpaRepository.java
@@ -4,4 +4,6 @@ import org.recordy.server.record_stat.domain.BookmarkEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookmarkJpaRepository extends JpaRepository<BookmarkEntity, Long> {
+
+    Long countAllByRecord_Id(long recordId);
 }
