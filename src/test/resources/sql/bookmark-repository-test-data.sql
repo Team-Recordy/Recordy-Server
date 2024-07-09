3614a0c44d1421e895c0e3a4c4ed4c58c1e2977b
elive7 <parksub020704@gmail.com>
2024-07-10 00:39:49 +0900

    [fix] BookmarkRepositoryIntegrationTest 수정 (#41)

--- src/test/resources/sql/bookmark-repository-test-data.sql
+++ src/test/resources/sql/bookmark-repository-test-data.sql
@@ -2,7 +2,7 @@ insert into `users` (`id`, `platform_id`, `platform_type`, `status`, `age_term`,
 values (1, 'abcdefg', 'KAKAO', 'ACTIVE', true, true, true, 'konu');
 
 insert into `users` (`id`, `platform_id`, `platform_type`, `status`, `age_term`, `personal_info_term`, `use_term`, `nickname`)
-values (1, 'abcdefgh', 'KAKAO', 'ACTIVE', true, true, true, 'subin');
+values (2, 'abcdefgh', 'KAKAO', 'ACTIVE', true, true, true, 'subin');
 
 insert into `keywords` (`id`, `keyword`)
 values (1, 'EXOTIC');
