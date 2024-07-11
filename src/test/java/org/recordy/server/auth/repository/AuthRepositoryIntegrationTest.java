621c5ee1390c876dd039fb2c492228fbc696e237
Jin Geonwoo <jinkonu99@gmail.com>
2024-07-11 14:35:02 +0900

    [test] Auth 리포지토리 테스트 수정 (#45)

--- src/test/java/org/recordy/server/auth/repository/AuthRepositoryIntegrationTest.java
+++ src/test/java/org/recordy/server/auth/repository/AuthRepositoryIntegrationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.recordy.server.auth.domain.Auth;
 import org.recordy.server.auth.domain.AuthPlatform;
+import org.recordy.server.auth.repository.impl.AuthRedisRepository;
 import org.recordy.server.util.DomainFixture;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,32 +14,33 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 public class AuthRepositoryIntegrationTest {
 
     @Autowired
     private AuthRepository authRepository;
+    @Autowired
+    private AuthRedisRepository authRedisRepository;
+
+    @BeforeEach
+    void tearDown() {
+        authRedisRepository.deleteAll();
+        System.out.println("shibal");
+        authRedisRepository.findAll().forEach(System.out::println);
+    }
 
     @Test
-    void save를_통해_인증_데이터를_저장할_수_있다() {
+    void save를_통해_Auth_객체를_저장할_수_있다() {
         // given
-        String id = "abc";
         boolean isSignedUp = true;
 
-        Auth auth = new Auth(
-                new AuthPlatform(id, AuthPlatform.Type.KAKAO),
-                DomainFixture.createAuthToken(),
-                isSignedUp
-        );
-
         // when
-        Auth result = authRepository.save(auth);
+        Auth result = authRepository.save(DomainFixture.createAuth(isSignedUp));
 
         // then
         assertAll(
-                () -> assertThat(result.getPlatform().getId()).isEqualTo(id),
+                () -> assertThat(result.getPlatform().getId()).isEqualTo(DomainFixture.PLATFORM_ID),
                 () -> assertThat(result.getPlatform().getType()).isEqualTo(AuthPlatform.Type.KAKAO),
                 () -> assertThat(result.getToken().getAccessToken()).isEqualTo(DomainFixture.ACCESS_TOKEN),
                 () -> assertThat(result.getToken().getRefreshToken()).isEqualTo(DomainFixture.REFRESH_TOKEN),
@@ -47,24 +49,40 @@ public class AuthRepositoryIntegrationTest {
     }
 
     @Test
-    void findByPlatformId를_통해_플랫폼_ID로_인증_데이터를_조회할_수_있다() {
+    void delete를_통해_Auth_객체를_삭제할_수_있다() {
         // given
-        String id = "abc";
-        boolean isSignedUp = true;
-
-        Auth auth = new Auth(
-                new AuthPlatform(id, AuthPlatform.Type.KAKAO),
-                DomainFixture.createAuthToken(),
-                isSignedUp
-        );
-        authRepository.save(auth);
+        Auth auth = authRepository.save(DomainFixture.createAuth(false));
 
         // when
-        Auth result = authRepository.findByPlatformId(id).orElse(null);
+        authRepository.delete(auth);
+
+        // then
+        assertThat(authRepository.findByPlatformId(auth.getPlatform().getId())).isEmpty();
+    }
+
+    @Test
+    void delete를_통해_존재하지_않는_Auth_객체를_삭제하더라도_에러가_발생하지_않는다() {
+        // given
+        Auth auth = DomainFixture.createAuth(false);
+
+        // when, then
+        assertThatCode(() -> authRepository.delete(auth))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void findByPlatformId를_통해_플랫폼_ID로_인증_데이터를_조회할_수_있다() {
+        // given
+        boolean isSignedUp = true;
+        authRepository.save(DomainFixture.createAuth(isSignedUp));
+
+        // when
+        Auth result = authRepository.findByPlatformId(DomainFixture.PLATFORM_ID)
+                .orElse(null);
 
         // then
         assertAll(
-                () -> assertThat(result.getPlatform().getId()).isEqualTo(id),
+                () -> assertThat(result.getPlatform().getId()).isEqualTo(DomainFixture.PLATFORM_ID),
                 () -> assertThat(result.getPlatform().getType()).isEqualTo(AuthPlatform.Type.KAKAO),
                 () -> assertThat(result.getToken().getAccessToken()).isEqualTo(DomainFixture.ACCESS_TOKEN),
                 () -> assertThat(result.getToken().getRefreshToken()).isEqualTo(DomainFixture.REFRESH_TOKEN),
@@ -82,28 +100,21 @@ public class AuthRepositoryIntegrationTest {
     }
 
     @Test
-    void ffindByRefreshToken를_통해_refresh_token으로_인증_데이터를_조회할_수_있다() {
+    void findByRefreshToken을_통해_refresh_token으로_인증_데이터를_조회할_수_있다() {
         // given
-        String id = "abc";
-        boolean isSignedUp = true;
-
-        Auth auth = new Auth(
-                new AuthPlatform(id, AuthPlatform.Type.KAKAO),
-                DomainFixture.createAuthToken(),
-                isSignedUp
-        );
-        authRepository.save(auth);
+        Auth auth = authRepository.save(DomainFixture.createAuth(true));
 
         // when
-        Auth result = authRepository.findByRefreshToken(DomainFixture.REFRESH_TOKEN).orElse(null);
+        Auth result = authRepository.findByRefreshToken(auth.getToken().getRefreshToken())
+                .orElse(null);
 
         // then
         assertAll(
-                () -> assertThat(result.getPlatform().getId()).isEqualTo(id),
+                () -> assertThat(result.getPlatform().getId()).isEqualTo(auth.getPlatform().getId()),
                 () -> assertThat(result.getPlatform().getType()).isEqualTo(AuthPlatform.Type.KAKAO),
                 () -> assertThat(result.getToken().getAccessToken()).isEqualTo(DomainFixture.ACCESS_TOKEN),
                 () -> assertThat(result.getToken().getRefreshToken()).isEqualTo(DomainFixture.REFRESH_TOKEN),
-                () -> assertThat(result.isSignedUp()).isEqualTo(isSignedUp)
+                () -> assertThat(result.isSignedUp()).isEqualTo(auth.isSignedUp())
         );
     }
 
@@ -114,43 +125,5 @@ public class AuthRepositoryIntegrationTest {
 
         //then
         assertThat(result).isEmpty();
-    }
-
-
-    @Test
-    void delete를_통해_Auth_객체를_삭제할_수_있다() {
-        // given
-        String id = "abc";
-        boolean isSignedUp = true;
-
-        Auth auth = new Auth(
-                new AuthPlatform(id, AuthPlatform.Type.KAKAO),
-                DomainFixture.createAuthToken(),
-                isSignedUp
-        );
-        authRepository.save(auth);
-
-        // when
-        authRepository.delete(auth);
-
-        // then
-        assertThat(authRepository.findByPlatformId(id)).isEmpty();
-    }
-
-    @Test
-    void delete를_통해_존재하지_않는_Auth_객체를_삭제하더라도_에러가_발생하지_않는다() {
-        // given
-        String id = "abc";
-        boolean isSignedUp = true;
-
-        Auth auth = new Auth(
-                new AuthPlatform(id, AuthPlatform.Type.KAKAO),
-                DomainFixture.createAuthToken(),
-                isSignedUp
-        );
-
-        // when, then
-        assertThatCode(() -> authRepository.delete(auth))
-                .doesNotThrowAnyException();
     }
 }
