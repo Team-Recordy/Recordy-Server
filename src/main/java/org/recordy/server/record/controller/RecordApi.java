b6e30a09c999ef42df5252f6dd268faf634f2fcb
sebbbin <nsb08045@daum.net>
2024-07-13 04:09:44 +0900

    [feat] api 명세서 추가(#56)

--- src/main/java/org/recordy/server/record/controller/RecordApi.java
+++ src/main/java/org/recordy/server/record/controller/RecordApi.java
@@ -4,15 +4,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.recordy.server.auth.security.UserId;
 import org.recordy.server.common.message.ErrorMessage;
-import org.recordy.server.record.controller.dto.RecordCreateRequest;
+import org.recordy.server.record.controller.dto.request.RecordCreateRequest;
 import org.recordy.server.record.domain.File;
 import org.recordy.server.record.domain.Record;
 import org.springframework.data.domain.Slice;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -82,7 +80,7 @@ public interface RecordApi {
             @UserId Long uploaderId,
             @RequestPart RecordCreateRequest recordCreateRequest,
             @RequestPart File file
-    );
+    ) ;
 
     @Operation(
             summary = "레코드 삭제 API",
@@ -223,4 +221,41 @@ public interface RecordApi {
             @RequestParam long cursorId,
             @RequestParam int size
     );
+
+    @Operation(
+            summary = "레코드 시청 API",
+            description = "사용자가 특정 레코드를 시청했음을 기록합니다.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "요청이 성공적으로 처리되었습니다.",
+                            content = @Content
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Not Found - 존재하지 않는 사용자 또는 기록입니다.",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(
+                                            implementation = ErrorMessage.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal Server Error - 서버 내부 오류입니다.",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(
+                                            implementation = ErrorMessage.class
+                                    )
+                            )
+                    )
+            }
+    )
+    public ResponseEntity<Void> watch(
+            @UserId long userId,
+            @RequestParam long recordId
+    );
+
 }
