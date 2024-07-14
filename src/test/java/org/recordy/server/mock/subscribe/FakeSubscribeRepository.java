57dec9e55b75c458b9dae41ced53d25765dd360d
Jin Geonwoo <jinkonu99@gmail.com>
2024-07-14 17:47:06 +0900

    [test] 구독에 대한 가짜 리포지토리 및 서비스 테스트 추가 (#47)

---
+++ src/test/java/org/recordy/server/mock/subscribe/FakeSubscribeRepository.java
@@ -0,0 +1,77 @@
+package org.recordy.server.mock.subscribe;
+
+import org.recordy.server.subscribe.domain.Subscribe;
+import org.recordy.server.subscribe.repository.SubscribeRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FakeSubscribeRepository implements SubscribeRepository {
+
+    public long subscribeAutoIncrementId = 1L;
+    private final Map<Long, Subscribe> subscribes = new HashMap<>();
+
+    @Override
+    public void save(Subscribe subscribe) {
+        Subscribe realSubscribe = Subscribe.builder()
+                .id(subscribeAutoIncrementId)
+                .subscribingUser(subscribe.getSubscribingUser())
+                .subscribedUser(subscribe.getSubscribedUser())
+                .build();
+        subscribes.put(subscribeAutoIncrementId++, realSubscribe);
+    }
+
+    @Override
+    public void delete(long subscribingUserId, long subscribedUserId) {
+        subscribes.values()
+                .removeIf(subscribe ->
+                        subscribe.getSubscribingUser().getId() == subscribingUserId &&
+                                subscribe.getSubscribedUser().getId() == subscribedUserId
+                );
+    }
+
+    @Override
+    public Slice<Subscribe> findAllBySubscribingUserId(long subscribingUserId, long cursor, Pageable pageable) {
+        List<Subscribe> content = subscribes.values().stream()
+                .filter(subscribe ->
+                        subscribe.getSubscribingUser().getId() == subscribingUserId &&
+                                subscribe.getSubscribedUser().getId() < cursor
+                )
+                .sorted(Comparator.comparing(Subscribe::getId).reversed())
+                .toList();
+
+        if (content.size() <= pageable.getPageSize()) {
+            return new SliceImpl<>(content, pageable, false);
+        }
+
+        return new SliceImpl<>(content, pageable, true);
+    }
+
+    @Override
+    public boolean existsBySubscribingUserIdAndSubscribedUserId(long subscribingUserId, long subscribedUserId) {
+        return subscribes.values().stream()
+                .anyMatch(subscribe ->
+                        subscribe.getSubscribingUser().getId() == subscribingUserId &&
+                        subscribe.getSubscribedUser().getId() == subscribedUserId
+                );
+    }
+
+    @Override
+    public long countSubscribingUsers(long subscribedUserId) {
+        return subscribes.values().stream()
+                .filter(subscribe -> subscribe.getSubscribedUser().getId() == subscribedUserId)
+                .count();
+    }
+
+    @Override
+    public long countSubscribedUsers(long subscribingUserId) {
+        return subscribes.values().stream()
+                .filter(subscribe -> subscribe.getSubscribingUser().getId() == subscribingUserId)
+                .count();
+    }
+}
