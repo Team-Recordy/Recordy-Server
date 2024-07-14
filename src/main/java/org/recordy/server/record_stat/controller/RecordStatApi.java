991b46bd38e2ab1a949dd7a20ed4fc9f8d4f1ca3
sebbbin <nsb08045@daum.net>
2024-07-15 02:16:35 +0900

    [feat] api 명세서 작성(#56)

--- src/main/java/org/recordy/server/record_stat/controller/RecordStatApi.java
+++ src/main/java/org/recordy/server/record_stat/controller/RecordStatApi.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.recordy.server.auth.security.UserId;
+import org.recordy.server.common.message.ErrorMessage;
 import org.recordy.server.record.domain.Record;
 import org.recordy.server.record_stat.domain.usecase.Preference;
 import org.springframework.data.domain.Slice;
@@ -15,16 +16,37 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Tag(name = "통계 관련 API")
+@Tag(name = "RecordStat Api")
 public interface RecordStatApi {
 
     @Operation(
-            summary = "북마크 API",
-            description = "북마크를 진행하는 API입니다. ",
+            summary = "레코드 북마크 API",
+            description = "사용자가 특정 레코드를 북마크합니다.",
             responses = {
                     @ApiResponse(
                             responseCode = "200",
-                            description = "성공"
+                            description = "요청이 성공적으로 처리되었습니다.",
+                            content = @Content
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Not Found - 존재하지 않는 사용자 또는 레코드입니다.",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(
+                                            implementation = ErrorMessage.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal Server Error - 서버 내부 오류입니다.",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(
+                                            implementation = ErrorMessage.class
+                                    )
+                            )
                     )
             }
     )
@@ -34,13 +56,33 @@ public interface RecordStatApi {
     );
 
     @Operation(
-            security = @SecurityRequirement(name = "Authorization"),
-            summary = "북마크 삭제 API",
-            description = "access token을 바탕으로 로그아웃을 진행하는 API입니다.",
+            summary = "레코드 북마크 삭제 API",
+            description = "사용자가 특정 레코드의 북마크를 삭제합니다.",
             responses = {
                     @ApiResponse(
                             responseCode = "204",
-                            description = "요청이 성공했습니다."
+                            description = "요청이 성공적으로 처리되었습니다.",
+                            content = @Content
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Not Found - 존재하지 않는 사용자 또는 레코드입니다.",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(
+                                            implementation = ErrorMessage.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal Server Error - 서버 내부 오류입니다.",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(
+                                            implementation = ErrorMessage.class
+                                    )
+                            )
                     )
             }
     )
@@ -51,8 +93,8 @@ public interface RecordStatApi {
 
     @Operation(
             security = @SecurityRequirement(name = "Authorization"),
-            summary = "유저 취향 키워드 top 3 API",
-            description = "유저가 저장, 시청, 올린 영상을 바탕으로 유저 취향 키워드 top 3 API입니다.",
+            summary = "유저 취향 키워드 Top 3 API",
+            description = "유저가 저장, 시청, 올린 영상을 바탕으로 유저 취향 키워드를 반환합니다.",
             responses = {
                     @ApiResponse(
                             responseCode = "200",
@@ -61,6 +103,26 @@ public interface RecordStatApi {
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
                                     schema = @Schema(
                                             implementation = Preference.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized - 인증이 필요합니다.",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(
+                                            implementation = ErrorMessage.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal Server Error - 서버 내부 오류입니다.",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(
+                                            implementation = ErrorMessage.class
                                     )
                             )
                     )
@@ -76,11 +138,31 @@ public interface RecordStatApi {
             responses = {
                     @ApiResponse(
                             responseCode = "200",
-                            description = "성공",
+                            description = "요청이 성공적으로 처리되었습니다.",
                             content = @Content(
                                     mediaType = MediaType.APPLICATION_JSON_VALUE,
                                     schema = @Schema(
                                             implementation = Slice.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized - 인증이 필요합니다.",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(
+                                            implementation = ErrorMessage.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal Server Error - 서버 내부 오류입니다.",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(
+                                            implementation = ErrorMessage.class
                                     )
                             )
                     )
