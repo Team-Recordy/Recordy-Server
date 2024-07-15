f5991a78d6fa728a25a450a9a1b6636b265948ba
나세빈 <94737768+sebbbin@users.noreply.github.com>
2024-07-16 02:24:37 +0900

    [fix] 머지 에러 수정

--- src/main/java/org/recordy/server/record/controller/dto/request/RecordCreateRequest.java
+++ src/main/java/org/recordy/server/record/controller/dto/request/RecordCreateRequest.java
@@ -1,5 +1,7 @@
 package org.recordy.server.record.controller.dto.request;
 
+import org.recordy.server.record.service.dto.FileUrl;
+
 public record RecordCreateRequest(
         String location,
         String content,
