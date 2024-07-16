e2b27e25af3398c2424d7c88df5dc6f911803f27
konu (Jin Geonwoo) <jinkonu99@gmail.com>
2024-07-17 05:21:07 +0900

    [settings] deploy.sh 파일 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,12 +16,14 @@ if [ -z "$IS_GREEN_EXIST" ];then
   echo "### BLUE -> GREEN ####"
   echo ">>> pull green image"
   docker pull recordy/recordy:latest
+  echo ">>> remove old green container"
+  docker compose rm -fs green
   echo ">>> up green container"
   docker compose up -d green
   while [ 1 = 1 ]; do
-  echo ">>> green health check ..."
-  sleep 3
-  REQUEST=$(curl http://127.0.0.1:8082/actuator/health)
+    echo ">>> green health check ..."
+    sleep 3
+    REQUEST=$(curl http://127.0.0.1:8082/actuator/health)
     if [ -n "$REQUEST" ]; then
       echo ">>> health check success !"
       break;
@@ -40,6 +42,8 @@ else
   echo "### GREEN -> BLUE ###"
   echo ">>> pull blue image"
   docker pull recordy/recordy:latest
+  echo ">>> remove old blue container"
+  docker compose rm -fs blue
   echo ">>> up blue container"
   docker compose up -d blue
   while [ 1 = 1 ]; do
