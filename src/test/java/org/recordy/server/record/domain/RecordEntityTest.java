87397caf67b42ebd1919081a3462c72ad7b319fd
elive7 <parksub020704@gmail.com>
2024-07-29 20:11:08 +0900

    [feat] RecordEntity 테스트 코드 작성 (#94)

--- src/test/java/org/recordy/server/record/domain/RecordEntityTest.java
+++ src/test/java/org/recordy/server/record/domain/RecordEntityTest.java
@@ -1,10 +1,14 @@
 package org.recordy.server.record.domain;
 
 import org.junit.jupiter.api.Test;
+import org.recordy.server.bookmark.domain.BookmarkEntity;
+import org.recordy.server.keyword.domain.Keyword;
+import org.recordy.server.keyword.domain.KeywordEntity;
 import org.recordy.server.record.service.dto.FileUrl;
 import org.recordy.server.user.domain.UserEntity;
 import org.recordy.server.user.domain.UserStatus;
 import org.recordy.server.util.DomainFixture;
+import org.recordy.server.view.domain.ViewEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -108,4 +112,91 @@ class RecordEntityTest {
                 () -> assertThat(recordEntity.toDomain().isUploader(100)).isFalse()
         );
     }
+
+    @Test
+    void addUpload를_통해_업로드_엔티티를_uploads_리스트에_추가할_수_있다() {
+        //given
+        RecordEntity recordEntity = RecordEntity.builder()
+                .videoUrl(DomainFixture.VIDEO_URL)
+                .thumbnailUrl(DomainFixture.THUMBNAIL_URL)
+                .location(DomainFixture.LOCATION)
+                .content(DomainFixture.CONTENT)
+                .user(DomainFixture.createUserEntity())
+                .build();
+
+        UploadEntity uploadEntity = UploadEntity.builder()
+                .record(recordEntity)
+                .keyword(KeywordEntity.from(Keyword.감각적인))
+                .build();
+
+        //when
+        recordEntity.addUpload(uploadEntity);
+
+        //then
+        assertAll(
+                () -> assertThat(recordEntity.getUploads().size()).isEqualTo(1),
+                () -> assertThat(recordEntity.getUploads().get(0).getRecord().getId()).isEqualTo(recordEntity.getId()),
+                () -> assertThat(recordEntity.getUploads().get(0).getKeyword().getKeyword()).isEqualTo(Keyword.감각적인)
+        );
+    }
+
+    @Test
+    void addView를_통해_View_엔티티를_views_리스트에_추가할_수_있다() {
+        //given
+        UserEntity userEntity = DomainFixture.createUserEntity();
+
+        RecordEntity recordEntity = RecordEntity.builder()
+                .videoUrl(DomainFixture.VIDEO_URL)
+                .thumbnailUrl(DomainFixture.THUMBNAIL_URL)
+                .location(DomainFixture.LOCATION)
+                .content(DomainFixture.CONTENT)
+                .user(DomainFixture.createUserEntity())
+                .build();
+
+        ViewEntity viewEntity = ViewEntity.builder()
+                .id(1L)
+                .record(recordEntity)
+                .user(userEntity)
+                .build();
+
+        //when
+        recordEntity.addView(viewEntity);
+
+        //then
+        assertAll(
+                () -> assertThat(recordEntity.getViews().size()).isEqualTo(1),
+                () -> assertThat(recordEntity.getViews().get(0).getRecord().getId()).isEqualTo(recordEntity.getId()),
+                () -> assertThat(recordEntity.getViews().get(0).getUser().getId()).isEqualTo(userEntity.getId())
+        );
+    }
+
+    @Test
+    void addBookmark를_통해_Bookmark_엔티티를_bookmarks_리스트에_추가할_수_있다() {
+        //given
+        UserEntity userEntity = UserEntity.from(DomainFixture.createUser(2));
+
+        RecordEntity recordEntity = RecordEntity.builder()
+                .videoUrl(DomainFixture.VIDEO_URL)
+                .thumbnailUrl(DomainFixture.THUMBNAIL_URL)
+                .location(DomainFixture.LOCATION)
+                .content(DomainFixture.CONTENT)
+                .user(DomainFixture.createUserEntity())
+                .build();
+
+        BookmarkEntity bookmarkEntity = BookmarkEntity.builder()
+                .record(recordEntity)
+                .user(userEntity)
+                .build();
+
+        //when
+        recordEntity.addBookmark(bookmarkEntity);
+
+        //then
+        assertAll(
+                () -> assertThat(recordEntity.getBookmarks().size()).isEqualTo(1),
+                () -> assertThat(recordEntity.getBookmarks().get(0).getRecord().getId()).isEqualTo(recordEntity.getId()),
+                () -> assertThat(recordEntity.getBookmarks().get(0).getUser().getId()).isEqualTo(userEntity.getId())
+        );
+    }
+
 }
