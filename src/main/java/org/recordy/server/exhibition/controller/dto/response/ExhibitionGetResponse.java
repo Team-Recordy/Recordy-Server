6dbbf70c9084988d47c7d426690312e746043ad7
Jin Geonwoo <jinkonu99@gmail.com>
2024-09-03 00:06:08 +0900

    [feat] ExhibitionGetResponse 추가 (#109)

---
+++ src/main/java/org/recordy/server/exhibition/controller/dto/response/ExhibitionGetResponse.java
@@ -0,0 +1,16 @@
+package org.recordy.server.exhibition.controller.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDate;
+
+public record ExhibitionGetResponse(
+        Long id,
+        String name,
+        String address,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+        LocalDate startDate,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+        LocalDate endDate
+) {
+}
