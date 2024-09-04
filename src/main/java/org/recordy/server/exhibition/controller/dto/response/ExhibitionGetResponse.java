7c295d2fa15650ea0673dc9096d8a4298ddd54ce
Jin Geonwoo <jinkonu99@gmail.com>
2024-09-04 18:12:52 +0900

    [refactor] ExhibitionGetResponse에서 위치 정보 제거 (#109)

--- src/main/java/org/recordy/server/exhibition/controller/dto/response/ExhibitionGetResponse.java
+++ src/main/java/org/recordy/server/exhibition/controller/dto/response/ExhibitionGetResponse.java
@@ -1,14 +1,12 @@
 package org.recordy.server.exhibition.controller.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import org.recordy.server.location.controller.dto.response.LocationGetResponse;
 
 import java.time.LocalDate;
 
 public record ExhibitionGetResponse(
         Long id,
         String name,
-        LocationGetResponse location,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
         LocalDate startDate,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
