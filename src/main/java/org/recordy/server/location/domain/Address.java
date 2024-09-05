af8d26851308724e7a3edb48893c7a6233244e33
Jin Geonwoo <jinkonu99@gmail.com>
2024-09-06 00:02:45 +0900

    [refactor] Address 내 getter 추가 (#109)

--- src/main/java/org/recordy/server/location/domain/Address.java
+++ src/main/java/org/recordy/server/location/domain/Address.java
@@ -2,11 +2,13 @@ package org.recordy.server.location.domain;
 
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor(staticName = "of")
 @NoArgsConstructor
 @Embeddable
+@Getter
 public class Address {
 
     private String formatted;
