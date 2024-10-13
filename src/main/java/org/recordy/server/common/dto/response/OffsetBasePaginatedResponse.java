a4c6d4ea7629668d33bfdf7ca0e4a7247901cdf3
Jin Geonwoo <jinkonu99@gmail.com>
2024-10-13 17:17:44 +0900

    [feat] offset 기반 페이지네이션 공통 응답 추가 (#113)

---
+++ src/main/java/org/recordy/server/common/dto/response/OffsetBasePaginatedResponse.java
@@ -0,0 +1,20 @@
+package org.recordy.server.common.dto.response;
+
+import org.springframework.data.domain.Slice;
+
+import java.util.List;
+
+public record OffsetBasePaginatedResponse<T>(
+        int pageNumber,
+        boolean hasNext,
+        List<T> data
+) {
+
+    public static <T> OffsetBasePaginatedResponse<T> of(Slice<T> slice) {
+        return new OffsetBasePaginatedResponse<>(
+                slice.getNumber(),
+                slice.hasNext(),
+                slice.getContent()
+        );
+    }
+}
