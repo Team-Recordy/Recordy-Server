4099804f45e7301038b4cca43c6ecf14111fede4
Jin Geonwoo <jinkonu99@gmail.com>
2024-10-27 18:24:15 +0900

    [hotfix] chore: ExhibitionDataInitializer 상태 off

--- src/main/java/org/recordy/server/common/util/data/ExhibitionDataInitializer.java
+++ src/main/java/org/recordy/server/common/util/data/ExhibitionDataInitializer.java
@@ -35,7 +35,7 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Slf4j
 //@Profile({"dev", "local"})
-@Component
+//@Component
 public class ExhibitionDataInitializer {
 
     private final ExhibitionRepository exhibitionRepository;
