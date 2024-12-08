3a6395693f0da68e2723ba279317fb3953e7a4f3
jin.geonwoo <jin.geonwoo@gmail.com>
2024-12-08 20:09:57 +0900

    [feat] ReportRepository.create() 구현 (#121)
    - ReportRepositoryImpl 정의 및 ReportJpaRepository.save() 호출
    - ReportJpaRepository 정의

---
+++ src/main/java/org/recordy/server/report/repository/impl/ReportRepositoryImpl.java
@@ -0,0 +1,18 @@
+package org.recordy.server.report.repository.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.recordy.server.report.domain.Report;
+import org.recordy.server.report.repository.ReportRepository;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class ReportRepositoryImpl implements ReportRepository {
+
+    private final ReportJpaRepository reportJpaRepository;
+
+    @Override
+    public void save(Report report) {
+        reportJpaRepository.save(report);
+    }
+}
