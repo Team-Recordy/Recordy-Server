9467a851ac48a56a3cce28fbc4c62fca98bd7620
elive7 <parksub020704@gmail.com>
2024-12-17 19:10:55 +0900

    [hotfix] chore: 버그 해결용 출력문 추가

--- src/main/java/org/recordy/server/slack/controller/SlackController.java
+++ src/main/java/org/recordy/server/slack/controller/SlackController.java
@@ -24,8 +24,9 @@ public class SlackController {
     @PostMapping
     public ResponseEntity<Void> handleInteractiveMessage(HttpServletRequest request) {
         try {
-            System.out.println("request = " + request);;
+            System.out.println("request = " + request);
             HttpServletRequest cachingRequest = new ContentCachingRequestWrapper(request);
+            System.out.println("cachingRequest = " + cachingRequest);
             Slack slack = new Slack(cachingRequest);
             String actionId = slack.getActionId();
 
