9deff27b91e1acc577ea3e1b28b2b879c2e89fd5
Jin Geonwoo <jinkonu99@gmail.com>
2025-01-12 19:38:44 +0900

    [hotfix] settings: @EnableScheduling을 첨부한 configuration 클래스 추가

---
+++ src/main/java/org/recordy/server/common/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package org.recordy.server.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@Configuration
+public class SchedulerConfig {
+}
