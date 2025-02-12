c8d03d4b89f8f15e2c91b160273959ec5c0de506
elive7 <parksub020704@gmail.com>
2025-02-12 14:40:52 +0900

    [hotfix] user from 내 순환 참조 문제 해결

--- src/main/java/org/recordy/server/user/domain/User.java
+++ src/main/java/org/recordy/server/user/domain/User.java
@@ -5,18 +5,15 @@ import static org.recordy.server.user.domain.UserStatus.ACTIVE;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import org.recordy.server.auth.domain.AuthPlatform;
 import org.recordy.server.common.message.ErrorMessage;
 import org.recordy.server.subscribe.domain.Subscribe;
-import org.recordy.server.subscribe.domain.SubscribeEntity;
 import org.recordy.server.user.domain.usecase.UserSignUp;
 import org.recordy.server.user.domain.usecase.UserUpdate;
 import org.recordy.server.user.exception.UserException;
@@ -49,15 +46,6 @@ public class User {
                 .termsAgreement(TermsAgreement.of(entity.isUseTerm(), entity.isPersonalInfoTerm(), entity.isAgeTerm()))
                 .createdAt(entity.getCreatedAt())
                 .updatedAt(entity.getUpdatedAt())
-                .subscribers(entity.getSubscribers() != null ? entity.getSubscribers()
-                    .stream()
-                    .map(SubscribeEntity::toDomain)
-                    .collect(Collectors.toList()) : new ArrayList<>())
-                .subscribings(entity.getSubscribings() != null ? entity.getSubscribings()
-                    .stream()
-                    .map(SubscribeEntity::toDomain)
-                    .collect(Collectors.toList()) : new ArrayList<>())
-
                 .build();
 
         return user;
